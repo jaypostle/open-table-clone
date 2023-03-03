d4016ddb9c38251974eec13c450090f560b3dc25
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-03-03 11:51:55 -0800

    created nested layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import Rating from "./components/Rating";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
@@ -10,21 +9,16 @@ import ReservationCard from "./components/ReservationCard";
 function RestaurantDetails() {
   return (
     <>
-      <Header />
-
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Title />
-          <Rating />
-          <Description />
-
-          <Images />
-          <Reviews />
-        </div>
-        <div className="w-[27%] relative text-reg">
-          <ReservationCard />
-        </div>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
       </div>
     </>
   );
