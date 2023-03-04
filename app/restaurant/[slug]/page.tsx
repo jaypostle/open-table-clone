59acb901fdf5e4ff93b89925963761f64948f83e
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-03-04 11:13:24 -0800

    populating data in restaurant page, slug, header, and layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -41,15 +41,15 @@ const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
 async function RestaurantDetails({ params }: { params: { slug: string } }) {
   const restaurant = await fetchRestaurantBySlug(params.slug);
 
-  console.log({ restaurant });
+  // console.log({ restaurant });
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images} name={restaurant.name} />
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
