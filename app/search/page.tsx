b449412238baf6e34ded34d222a9d1a814f32d1a
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-03-04 14:57:39 -0800

    fetch search data and populating search wall with proper ts types

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,14 +1,54 @@
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSidebar from "./components/SearchSidebar";
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
 
-function Search() {
+const prisma = new PrismaClient();
+
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+}
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+
+  if (!city) return prisma.restaurant.findMany({ select });
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+async function Search({ searchParams }: { searchParams: { city: string } }) {
   // extract query params
+  // console.log(searchParams.city);
 
   // fetch for all restaurants with location = city
-  // ask only for data you need
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
 
+  // ask only for data you need
   // if there is no data show a no restaurants found message
+
   return (
     <>
       <Header />
@@ -16,7 +56,13 @@ function Search() {
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSidebar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length > 1 ? (
+            restaurants.map((restaurant) => (
+              <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+            ))
+          ) : (
+            <p>Sorry, we found no restaurants in this area.</p>
+          )}
         </div>
       </div>
     </>
