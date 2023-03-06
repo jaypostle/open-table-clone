b1ca0e5e2c2a16b18d454380cb985b6eb9ca2d00
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-03-05 20:17:20 -0800

    added reviews to main page and menu page

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -6,11 +6,9 @@ interface Props {
   restaurant: RestaurantCardType;
 }
 export default function RestaurantCard({ restaurant }: Props) {
+  console.log(restaurant);
   return (
-    <div
-      className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
-      key={restaurant.id}
-    >
+    <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
       <Link href={`/restaurant/${restaurant.slug}`}>
         <img
           src={restaurant.main_image}
@@ -21,7 +19,10 @@ export default function RestaurantCard({ restaurant }: Props) {
           <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
             <div className="flex mb-2">*****</div>
-            <p className="ml-2">77 reviews</p>
+            <p className="ml-2">
+              {restaurant.reviews.length} review
+              {restaurant.reviews.length === 1 ? "" : "s"}
+            </p>
           </div>
           <div className="flex text-reg font-light capitalize">
             <p className=" mr-3">{restaurant.cuisine.name}</p>
--- app/restaurant/[slug]/components/Rating.tsx
+++ app/restaurant/[slug]/components/Rating.tsx
@@ -1,12 +1,18 @@
-export default function Rating() {
+import { Review } from "@prisma/client";
+
+export default function Rating({ reviews }: { reviews: Review[] }) {
+  const ratingAverage = reviews.reduce((r, c) => r + c.rating, 0);
+
   return (
     <div className="flex items-end">
       <div className="ratings mt-2 flex items-center">
         <p>*****</p>
-        <p className="text-reg ml-3">4.9</p>
+        <p className="text-reg ml-3">{ratingAverage}</p>
       </div>
       <div>
-        <p className="text-reg ml-4">600 Reviews</p>
+        <p className="text-reg ml-4">
+          {reviews.length} Review{reviews.length === 1 ? "" : "s"}
+        </p>
       </div>
     </div>
   );
