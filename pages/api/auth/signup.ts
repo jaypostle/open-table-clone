8b82a3fc8ab088e2db1c09889d88cc0844d74e9c
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-03-08 16:27:14 -0800

    sign up endpoint created with jwt token, added libraries jose, bcrypt, validator

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
 
 const prisma = new PrismaClient();
 
@@ -70,6 +72,30 @@ export default async function handler(
           "There is already an account associated with this email address.",
       });
     }
-    res.status(200).json({ hello: "body" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        city,
+        phone,
+        email,
+      },
+    });
+
+    const alg = "HS256";
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    const token = await new jose.SignJWT({
+      email: user.email,
+    })
+      .setProtectedHeader({ alg })
+      .setExpirationTime("24h")
+      .sign(secret);
+
+    res.status(200).json({ hello: token });
   }
 }
