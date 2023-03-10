f67e95cce2e9b75b05c1d48b28abc86896187462
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-03-10 09:10:51 -0800

    create useAuth hook

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import AuthModalInputs from "./AuthModalInputs";
+import useAuth from "../../hooks/useAuth";
 
 const style = {
   position: "absolute" as "absolute",
@@ -22,6 +23,7 @@ export default function AuthModal({ isSignIn }: { isSignIn: boolean }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const { signin } = useAuth();
 
   const renderContent = (signinContent: string, signupContent: string) => {
     return isSignIn ? signinContent : signupContent;
@@ -68,6 +70,12 @@ export default function AuthModal({ isSignIn }: { isSignIn: boolean }) {
     });
   };
 
+  const handleClick = () => {
+    if (isSignIn) {
+      signin({ email: inputs.email, password: inputs.password });
+    }
+  };
+
   return (
     <div>
       <Button
@@ -107,6 +115,7 @@ export default function AuthModal({ isSignIn }: { isSignIn: boolean }) {
                 isSignIn={isSignIn}
               />
               <button
+                onClick={handleClick}
                 disabled={disabled}
                 className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400"
               >
